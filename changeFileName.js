b1f2ecb56e0ec81a53af41d5a8b082c03d5da6e2
Thang Lam <thanglhssi@gmail.com>
2020-08-26 21:15:28 +0700

    update

--- changeFileName.js
+++ changeFileName.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 
-var i=10;
-for(i=10;i< 50;i++){
-    fs.rename('/Volumes/data/Works/english/srcEng/eng-story/english/ff1/1-' + i +' Family and Friends 2e Students MultiROM 1.mp3', 
-    '/Volumes/data/Works/english/srcEng/eng-story/english/ff1/0'+i+'.mp3', function(err) {
+var i=1;
+for(i=1;i< 10;i++){
+    fs.rename('/Volumes/data/Works/english/srcEng/eng-story/english/everyDayLifeSentenceCard/0' + i +' Track ' + i +'.mp3', 
+    '/Volumes/data/Works/english/srcEng/eng-story/english/everyDayLifeSentenceCard/0'+i+'.mp3', function(err) {
         if ( err ) console.log('ERROR: ' + err);
     });
 }
